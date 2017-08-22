c42cc2caa0f08bd0d9230ebe9b682dfe6910bbee
LuciferTM <roy_qian@gmail.com>
2017-08-22 14:41:12 +0800

    获取规格参数

---
+++ taotao-parent/taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/ItemParamItemService.java
@@ -0,0 +1,8 @@
+package com.taotao.service;
+
+/**
+ * Created by LuciferTM on 2017/8/22.
+ */
+public interface ItemParamItemService {
+    String getItemParamByItemId(Long itemId);
+}
--- taotao-parent/taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/impl/ItemParamServiceImpl.java
+++ taotao-parent/taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/impl/ItemParamServiceImpl.java
@@ -6,6 +6,7 @@ import com.taotao.pojo.TbItemParam;
 import com.taotao.pojo.TbItemParamExample;
 import com.taotao.service.ItemParamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 /**
  * Created by LuciferTM on 2017/8/15.
  */
+@Service
 public class ItemParamServiceImpl implements ItemParamService {
 
     @Autowired
