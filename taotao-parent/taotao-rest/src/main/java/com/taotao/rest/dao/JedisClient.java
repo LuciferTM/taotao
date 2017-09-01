e8e4a576b261407897bb9cb293ccc0291ccf4717
LuciferTM <roy_qian@gmail.com>
2017-09-01 12:38:09 +0800

    JedisClient 已经 bean注入， 没有标明@Service， 使用的手动注入，因为可能会在单机版和集群板中切换

---
+++ taotao-parent/taotao-rest/src/main/java/com/taotao/rest/dao/JedisClient.java
@@ -0,0 +1,21 @@
+package com.taotao.rest.dao;
+
+/**
+ * <p>Title:Jedis客户端接口</p>
+ * <p>Description: </p>
+ * <p>Company:</p>
+ *
+ * @author Lucifer
+ * @date 2017/9/1
+ */
+public interface JedisClient {
+    String get(String key);
+    String set(String key, String value);
+    String hget(String hkey, String key);
+    long hset(String hkey, String key, String value);
+    long incr(String key);
+    long expire(String key, int second);
+    long ttl(String key);
+    long del(String key);
+    long hdel(String hkey, String key);
+}
