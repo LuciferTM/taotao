f1aef929cae14bb0d7aeb75654d87c9e35f196e2
LuciferTM <roy_qian@gmail.com>
2017-09-01 16:35:20 +0800

    设置缓存功能

--- taotao-parent/taotao-rest/src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
+++ taotao-parent/taotao-rest/src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
@@ -1,12 +1,15 @@
 package com.taotao.rest.service.impl;
 
+import com.taotao.common.util.JsonUtils;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
+import com.taotao.rest.dao.JedisClient;
 import com.taotao.rest.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
+import org.apache.commons.lang3.StringUtils;
 import java.util.List;
 
 /**
@@ -20,15 +23,41 @@ import java.util.List;
 @Service
 public class ContentServiceImpl implements ContentService {
 
+   @Value("${INDEX_CONTENT_REDIS_KEY}")
+   private String INDEX_CONTENT_REDIS_KEY;
+
     @Autowired
     private TbContentMapper tbContentMapper;
 
+    @Autowired
+    private JedisClient jedisClient;
+
     @Override
     public List<TbContent> getContentList(long contentCid) {
+
+        //取缓存
+        try {
+            String result = jedisClient.hget(INDEX_CONTENT_REDIS_KEY, contentCid+"");
+            if(!StringUtils.isBlank(result)){
+                List<TbContent> list = JsonUtils.jsonToList(result, TbContent.class);
+                return list;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
         TbContentExample tbContentExample = new TbContentExample();
         TbContentExample.Criteria criteria = tbContentExample.createCriteria();
         criteria.andCategoryIdEqualTo(contentCid);
         List<TbContent> list = tbContentMapper.selectByExample(tbContentExample);
+
+        //设置缓存
+        try{
+            String jsonList = JsonUtils.objectToJson(list);
+            Long result = jedisClient.hset(INDEX_CONTENT_REDIS_KEY, contentCid+"", jsonList);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         return list;
     }
 }
