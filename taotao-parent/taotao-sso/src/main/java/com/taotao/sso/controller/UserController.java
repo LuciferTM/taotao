0e6b3137287e91bfa168553dc766f8e3b9aa9e52
LuciferTM <roy_qian@gmail.com>
2017-09-20 13:51:33 +0800

    将check user data接口设置为支持jsonp，使用MappingJacksonValue

--- taotao-parent/taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-parent/taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
@@ -6,6 +6,7 @@ import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * <p>Title:</p>
+ * <p>Title:验证用户数据的接口</p>
  * <p>Description: </p>
- * <p>Company:</p>
+ *
+ * 这里要注意的就是 springmvc支持jsonp使用MappingJacksonValue
+ * example:
+ *
+ *  @RequestMapping("directives.json")
+    @ResponseBody
+    public MappingJacksonValue directives(String callback) {
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue("you can put everything here.");
+        mappingJacksonValue.setJsonpFunction(callback);
+        return mappingJacksonValue;
+ }
  *
  * @author Lucifer
  * @date 2017/9/19
@@ -30,23 +41,39 @@ public class UserController {
     @RequestMapping("/check/{param}/{type}")
     @ResponseBody
     public Object checkData(@PathVariable String param, @PathVariable Integer type, String callback){
+        TaotaoResult result = null;
         if(StringUtils.isBlank(param)){
-            return TaotaoResult.build(400, "内容不能为空");
+            result = TaotaoResult.build(400, "内容不能为空");
         }
         if(type == null){
-            return TaotaoResult.build(400, "type不能为空");
+            result = TaotaoResult.build(400, "type不能为空");
         }
+        else if(type < 1 || type > 3){
+            result = TaotaoResult.build(400, "Invalid Input");
+        }
+        if(result != null){
+            if(callback != null){
+                MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+                mappingJacksonValue.setJsonpFunction(callback);
+                return mappingJacksonValue;
+            } else {
+                return result;
+            }
 
-        if(type < 1 || type > 3){
-            return TaotaoResult.build(400, "Invalid Input");
         }
-        TaotaoResult result = null;
         try {
             result = userService.checkData(param, type);
         } catch (Exception e) {
             e.printStackTrace();
             result = TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
         }
+
+        if(callback != null){
+            MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+            mappingJacksonValue.setJsonpFunction(callback);
+            return mappingJacksonValue;
+        }
+
         return result;
     }
 
@@ -61,7 +88,6 @@ public class UserController {
             return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
         }
     }
-
 
 
 }
